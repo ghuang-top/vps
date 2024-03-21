8e16029987cb4041424b68fd7a2f19c5339d513c
ghuan <1036636139@qq.com>
2024-03-22 00:04:55 +0800

    03-X-UI

--- install/03-X-UI.sh
+++ install/03-X-UI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# 03-X-UI
 
 # VPS Initialization
-sudo -i # 切换到root用户
 apt update -y && apt upgrade -y  # 更新一下包
 apt install wget curl sudo vim git lsof -y # Debian系统比较干净，安装常用的软件
 
@@ -28,12 +28,8 @@ EOF
 
 # ctrl+x退出，按y保存，enter确认
 
-# 运行docker-compose
-# 查看端口是否被占用
-lsof -i:8030  # 80
-lsof -i:54321  # 80
 
-# 运行
+# 运行docker
 docker-compose up -d 
 
 # 打开防火墙的端口
@@ -46,12 +42,4 @@ echo "访问链接:"
 echo "IP: ip:54321"
 echo "Email: admin"
 echo "Password: admin"
-echo "修改X-UI账号信息:"
-echo "面板监听端口: 8030"
-echo "面板URL根路径: /nSAsXv/"
-echo "Email: ghuang0425@gmail.com"
-echo "Password: gmail.com"
-echo "IP: ip:8030/nSAsXv"
-echo "http://74.48.16.14:8030/nSAsXv/xui/"
-echo "反向代理链接:"
-echo "根路径:/GbRY2nq/ https://xui.ghuang.top/nSAsXv/"
+echo "注意面板URL根路径: /nSAsXv/"
