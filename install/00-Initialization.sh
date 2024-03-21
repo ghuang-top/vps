ccc326902e3f0c3fffeeaed9194c4b25c0866fa0
ghuan <1036636139@qq.com>
2024-03-21 23:44:00 +0800

    00-Initialization

--- install/00-Initialization.sh
+++ install/00-Initialization.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 # 00-Initialization
+
 # VPS Initialization
-apt update -y && apt upgrade -y  #更新一下包
+apt update -y && apt upgrade -y  # 更新一下包
 apt install wget curl sudo vim git -y  # Debian系统比较干净，安装常用的软件
 
 # 安装防火墙
@@ -10,7 +10,7 @@ apt install wget curl sudo vim git -y  # Debian系统比较干净，安装常用
 apt install -y ufw
 
 # 2、开启防火墙
-ufw enable
+ufw --force enable
 
 # 3、开启SSH端口(22)
 ufw allow 22
@@ -19,16 +19,22 @@ ufw allow 443
 ufw status
 
 # 解决debian sudo问题
-apt install sudo
+apt install -y sudo
 sudo usermod -aG sudo root
 
-# 安装Docker
-# 1、安装docker
-curl -fsSL https://get.docker.com | sudo sh
+# 检查Docker和Docker-Compose是否已安装，如果未安装则安装它们
+if ! command -v docker &> /dev/null
+then
+    # 安装docker
+    curl -fsSL https://get.docker.com | sudo sh
+fi
 
-# 2、安装 Docker Compose
-apt install docker-compose -y
+if ! command -v docker-compose &> /dev/null
+then
+    # 安装 Docker Compose
+    apt install -y docker-compose
+fi
 
-# 3、查看Docker和Docker-Compose的版本
+# 查看Docker和Docker-Compose的版本
 docker --version
 docker-compose --version
