24982251ef0beecee23cc0ba23323989d5efc2b4
ghuan <1036636139@qq.com>
2024-03-22 11:45:16 +0800

    Check-docker-compose

---
+++ install/Check-docker-compose.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Check-docker-compose
+
+cd /root/data/docker_data/Nginx
+nano docker-compose.yml
+
+cd /root/data/docker_data/NginxProxy
+nano docker-compose.yml
+
+cd /root/data/docker_data/X-UI
+nano docker-compose.yml
+
+cd /root/data/docker_data/Frps
+nano docker-compose.yml
+
+cd /root/data/docker_data/Rustdesk
+nano docker-compose.yml
+
+cd /root/data/docker_data/Vaultwarden
+nano docker-compose.yml
+
+cd /root/data/docker_data/EasyImage
+nano docker-compose.yml
+
+cd /root/data/docker_data/Wordpress
+nano docker-compose.yml
+
+cd /root/data/docker_data/Nextcloud
+nano docker-compose.yml
+
+cd /root/data/docker_data/Joplin
+nano docker-compose.yml
+
+# ctrl+x退出，按y保存，enter确认
