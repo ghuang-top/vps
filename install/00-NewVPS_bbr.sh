2e858ad1388a3c2d9022d731187b6fc30721e3ae
ghuan <1036636139@qq.com>
2024-03-22 23:12:47 +0800

    # 安装 linux-xanmod-x64v2

--- install/00-NewVPS_bbr.sh
+++ install/00-NewVPS_bbr.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 # 检查操作系统类型和架构
 if [ -r /etc/os-release ]; then
     . /etc/os-release
@@ -21,10 +24,15 @@ fi
 # 检查是否已安装XanMod内核
 if dpkg -l | grep -q 'linux-xanmod'; then
     echo "XanMod内核已经安装，无需重复安装"
-    congestion_algorithm=$(sysctl -n net.ipv4.tcp_congestion_control)
-    queue_algorithm=$(sysctl -n net.core.default_qdisc)
-    echo "网络拥堵算法: $congestion_algorithm $queue_algorithm"
 else
+
+    # 安装 linux-xanmod-x64v2
+    apt update -y  && apt install -y nano
+    # 在文件末尾添加以下行：
+    echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee -a /etc/apt/sources.list
+    wget -qO - https://deb.xanmod.org/gpg.key | sudo apt-key --keyring /usr/share/keyrings/xanmod-archive-keyring.gpg add -
+    sudo apt update
+
     # 安装必要的软件并添加存储库
     apt update -y
     apt install -y wget gnupg
@@ -43,7 +51,17 @@ EOF
     # 清理临时文件
     rm -f /etc/apt/sources.list.d/xanmod-release.list
     rm -f check_x86-64_psabi.sh*
-    echo "XanMod内核安装并BBR3启用成功。重启后生效"
-    echo "现在重启"
-    reboot
+fi
+
+# 检查BBR3是否成功开启
+congestion_algorithm=$(sysctl -n net.ipv4.tcp_congestion_control)
+queue_algorithm=$(sysctl -n net.core.default_qdisc)
+
+if [[ "$congestion_algorithm" == "bbr" && "$queue_algorithm" == "fq_pie" ]]; then
+    echo "网络拥堵算法: $congestion_algorithm $queue_algorithm"
+else
+    echo "无法启用BBR3，正在重启以应用更改"
+    rm -f /etc/apt/sources.list.d/xanmod-release.list
+    rm -f check_x86-64_psabi.sh*
+    #reboot
 fi
