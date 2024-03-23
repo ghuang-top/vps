e913bf67048096fb8d821d6d62cfe9c45fce00a3
ghuan <1036636139@qq.com>
2024-03-23 15:56:05 +0800

    run_Fail2ban

--- install/00-Initialization.sh
+++ install/00-Initialization.sh
@@ -193,6 +193,37 @@ fi
 run_bbr
 
 
+run_Fail2ban(){
+# 1. 安装与启动 Fail2ban
+apt update -y && apt install -y fail2ban
+systemctl start fail2ban
+systemctl enable fail2ban
+systemctl status fail2ban --no-pager && echo "Continue with the next script" && exit 0
+
+# 2. 主配置文件操作
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+#nano /etc/fail2ban/jail.local
+#systemctl restart fail2ban
+
+# 3. 处理状态异常及 SSH 防御
+rm -rf /etc/fail2ban/jail.d/*
+# nano /etc/fail2ban/jail.d/sshd.local
+cat <<EOF > /etc/fail2ban/jail.d/sshd.local
+[sshd]
+enabled = true
+mode   = normal
+backend = systemd
+EOF
+
+# 4. 重启服务
+systemctl restart fail2ban
+#fail2ban-client status
+#fail2ban-client status sshd
+}
+run_Fail2ban
+
+
+
 echo "--------输出信息----------"
 echo "------------------------"
 echo "1、优化后的DNS地址为："
