eb9d00eff10c85ffe8f496a928b7a7344dd6f5e4
ghuan <1036636139@qq.com>
2024-03-23 19:36:42 +0800

    1

--- install/00-Initialization.sh
+++ install/00-Initialization.sh
@@ -62,12 +62,16 @@ vps_init
 
 echo "5、优化DNS地址为Cloudflare和Google"
 optimize_DNS(){
-    echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-    echo "nameserver 172.31.255.2" >> /etc/resolv.conf
-    echo "nameserver 74.82.42.42" >> /etc/resolv.conf
-    echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-    echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+# 删除旧的DNS地址
+sed -i '/^nameserver/d' /etc/resolv.conf
+
+# 添加新的DNS地址
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 172.31.255.2" >> /etc/resolv.conf
+echo "nameserver 74.82.42.42" >> /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 }
 optimize_DNS
 
