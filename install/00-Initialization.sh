7c5e13d2f29bee113ed7157570a056275edb50d7
ghuan <1036636139@qq.com>
2024-03-23 17:15:59 +0800

    run_banPing

--- install/00-Initialization.sh
+++ install/00-Initialization.sh
@@ -225,6 +225,15 @@ fi
 }
 run_bbr
 
+run_banPing(){
+# 替换before.rules文件中的echo-request规则
+sudo sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/g' /etc/ufw/before.rules
+
+# 重新加载UFW防火墙规则
+sudo ufw reload
+}
+run_banPing
+
 
 echo "--------输出信息----------"
 echo "------------------------"
