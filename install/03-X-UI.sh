a0990940707430d5075f6392a7cf91ca8de6a36d
ghuan <1036636139@qq.com>
2024-03-26 00:47:10 +0800

    1

--- install/03-X-UI.sh
+++ install/03-X-UI.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-# 03-X-UI
+# chmod +x 03-X-UI.sh && ./03-X-UI.sh
+# curl -sS -O https://raw.githubusercontent.com/ghuang-top/vps/main/install/03-X-UI.sh && chmod +x 03-X-UI.sh && ./03-X-UI.sh
 
-# VPS Initialization
+ipv4_address=$(curl -s ipv4.ip.sb)
+port80=54321
+port443=8100
+
+
+# 1、更新包
 apt update -y && apt upgrade -y  # 更新一下包
 
 
-# 创建X-UI安装目录
+# 2、创建X-UI安装目录
 mkdir -p /root/data/docker_data/X-UI
 cd /root/data/docker_data/X-UI
-# nano docker-compose.yml
 
-# 配置X-UI的docker-compose
+# 3、配置X-UI的docker-compose
 cat <<EOF > docker-compose.yml
 version: "3"
 services:
@@ -26,19 +31,20 @@ services:
         tty: true
 EOF
 
-# ctrl+x退出，按y保存，enter确认
-
-# 运行docker
+# 4、安装
 docker-compose up -d 
 
-# 打开防火墙的端口
-ufw allow 8030
-ufw allow 54321
+# 5、打开防火墙的端口
+ufw allow $port80
+ufw allow $port443
 ufw status
 
 # 打印访问链接
+echo "------------------------"
 echo "访问链接:"
-echo "IP: your_ip_address:54321"
+echo "http://$ipv4_address:$port80"
 echo "Email: admin"
 echo "Password: admin"
 echo "注意面板URL根路径: /nSAsXv/"
+echo "------------------------"
+
